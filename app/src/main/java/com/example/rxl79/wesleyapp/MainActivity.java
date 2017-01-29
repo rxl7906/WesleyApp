a8c84dc82c7596ce02ddb7bd235eaa6e9ed0da3e
rxl79 <rxl7906@rit.edu>
2017-01-28 21:00:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/rxl79/wesleyapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rxl79.wesleyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
